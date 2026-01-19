2125a78335e7d52d59075b25873b6f98b60d9b46
Andrew <andrebostan@gmail.com>
2026-01-19 12:28:26 +0300

    fixes 3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/bostan2048/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
